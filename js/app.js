961e90fa28b149933595ceb8e4712d1939804112
YoshiCity <pawel.mirys@gmail.com>
2021-10-19 21:10:02 +0200

    Modal Project added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
+// Filter Project Selectors
 const cards = document.querySelectorAll('.store-item');
 const btns = document.querySelectorAll('.filter-btn');
 const searchBar = document.querySelector('#search-item');
+// Modal Project Selectors
+let arr = [];
+let initIndex = 0;
+const images = document.querySelectorAll('.store-img');
+const lightBox = document.querySelector('.lightbox-container');
+const lightBoxItem = document.querySelector('.lightbox-item');
+const closeBtn = document.querySelector('.lightbox-close');
+const controls = document.querySelectorAll('.lightbox-control');
 
+// Filter Project
 btns.forEach((button) => {
   button.addEventListener('click', (el) => {
     el.preventDefault();
@@ -29,5 +39,45 @@ searchBar.addEventListener('input', (e) => {
     } else {
       card.style.display = 'none';
     }
+  });
+});
+
+// Modal Project
+
+images.forEach((img) => {
+  arr.push(img.src);
+});
+console.log(arr);
+
+cards.forEach((card) => {
+  card.addEventListener('click', (e) => {
+    let img = e.target.src;
+    initIndex = arr.indexOf(img);
+    lightBoxItem.style.backgroundImage = `url(${img})`;
+    lightBox.classList.add('show');
+    console.log(img);
+    console.log(initIndex);
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  lightBox.classList.contains('show')
+    ? lightBox.classList.remove('show')
+    : undefined;
+});
+
+controls.forEach((control) => {
+  control.addEventListener('click', (e) => {
+    let id = e.target.id;
+    console.log(id);
+    id === 'right' ? initIndex++ : initIndex--;
+    if (initIndex >= arr.length) {
+      initIndex = 0;
+    } else if (initIndex < 0) {
+      initIndex = arr.length - 1;
+    }
+    lightBoxItem.style.backgroundImage = `url(${arr[initIndex]})`;
+    console.log(initIndex);
+    console.log(arr[initIndex]);
   });
 });
